7952d98c424a8187f5f20a0967952133804ee718
chetanpal001 <chetan.pal868@gmail.com>
2024-05-06 17:06:49 +0530

    added placeholder code to integrate the planner and logger UIs

--- Frontend/planner/script.js
+++ Frontend/planner/script.js
@@ -4,9 +4,10 @@ const planButton = document.getElementById('plan-btn');
 const positiveIntegerPattern = /^[1-9]\d*$/;
 
 function togglePlanButton() {
+    const gender = document.querySelector('input[name="gender"]:checked');
     if (
         weightInput.value.trim() !== '' &&
-        positiveIntegerPattern.test(weightInput.value.trim())
+        positiveIntegerPattern.test(weightInput.value.trim() && && gender !== null)
     ) {
         planButton.disabled = false;
     } else {
@@ -15,3 +16,28 @@ function togglePlanButton() {
 }
 
 weightInput.addEventListener('input', togglePlanButton);
+
+const genderRadios = document.querySelectorAll('input[name="gender"]');
+genderRadios.forEach(radio => radio.addEventListener('change', togglePlanButton));
+
+planButton.addEventListener('click', async function handleClick() {
+  const weight = weightInput.value.trim();
+  const gender = document.querySelector('input[name="gender"]:checked').value; // Get gender value
+  const token = sessionStorage.getItem("token");
+
+  fetch('http://localhost:42000/api/workout/workoutPlan?gender=${gender}&weight=${weight}', {
+                 method: 'GET',
+                 headers: {
+                     'Content-Type': 'application/json',
+                     Authorization: `Bearer ${token}`
+                 }
+             })
+             .then(response => response.json())
+             .then(data => {
+               console.log(data);
+             })
+             .catch(error => {
+               console.error(error);
+             });
+  console.log("Add the code to target the div for displaying the workout plan");
+});
